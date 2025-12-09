684a34048d6807264c54b9de17b6ebec7aefeee8
ngocanh <nguyenngocanh2695@gmail.com>
2025-12-09 23:33:08 +0700

    View/layouts header,footer

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,17 +1,20 @@
 <nav>
     <a href="index.php">Trang chủ</a>
 
+
     <?php if (isset($_SESSION['user_id'])): ?>
         <span>Xin chào, <b><?php echo $_SESSION['username']; ?></b></span>
-        
+       
         <?php if ($_SESSION['role'] == 2): ?>
             <a href="index.php?controller=admin&action=index">Quản trị</a>
         <?php endif; ?>
 
-        <a href="index.php?controller=auth&action=logout" 
+
+        <a href="index.php?controller=auth&action=logout"
            style="color: red; margin-left: 10px;">
            Đăng xuất
         </a>
+
 
     <?php else: ?>
         <a href="index.php?controller=auth&action=login">Đăng nhập</a>
