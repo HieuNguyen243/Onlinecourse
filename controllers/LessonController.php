cd2c528f9c1394e8f618e4743accc3e987b06154
HieuNguyen243 <hieunguyen0816470@gmail.com>
2025-12-10 21:42:49 +0700

    UpdateLessonController

--- controllers/LessonController.php
+++ controllers/LessonController.php
@@ -14,16 +14,32 @@ class LessonController {
         $this->materialModel = new MaterialModel($pdo);
     }
 
+
     public function detail() {
         if (isset($_GET['lesson_id']) && isset($_SESSION['user_id'])) {
             $lesson_id = $_GET['lesson_id'];
+            $course_id = $_GET['course_id']; 
+            $student_id = $_SESSION['user_id'];
+
+            
             $material = $this->materialModel->getMaterialsByLesson($lesson_id);
+            
+            
+            $listLessons = $this->lessonModel->getLessonsByCourse($course_id);
+            
+            foreach ($listLessons as &$l) {
+                $l['completed'] = $this->lessonModel->isLessonCompleted($student_id, $l['id']);
+                $l['is_current'] = ($l['id'] == $lesson_id);
+            }
+            unset($l);
+
             require './views/lesson/detail.php';
         } else {
             header("Location: index.php?controller=auth&action=login");
             exit();
         }
     }
+
     public function complete() {
         if (isset($_POST['lesson_id']) && isset($_POST['course_id']) && isset($_SESSION['user_id'])) {
             $lesson_id = $_POST['lesson_id'];
